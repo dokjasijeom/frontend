c383ebed003aa8ffb541473ac6e88cc0df292dc8
kroongft <51918951+kroongft@users.noreply.github.com>
2023-11-09 19:38:56 +0900

    add: typography 테마 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,7 @@
+import { typography } from './typography'
 import { color } from './color'
 
-const theme = { color }
+const theme = { color, typography }
 
 export type Theme = typeof theme
 
---
+++ src/styles/typography.ts
@@ -0,0 +1,64 @@
+import { css } from 'styled-components'
+
+export const typography = {
+  head1: css`
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 132%; /* 26.4px */
+    letter-spacing: -0.4px;
+  `,
+  head2: css`
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 140%; /* 22.4px */
+    letter-spacing: -0.32px;
+  `,
+  head3: css`
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%; /* 22.4px */
+    letter-spacing: -0.32px;
+  `,
+  body1: css`
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%; /* 19.6px */
+    letter-spacing: -0.28px;
+  `,
+  body2: css`
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 19.6px */
+    letter-spacing: -0.28px;
+  `,
+  body3: css`
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 140%; /* 19.6px */
+    letter-spacing: -0.28px;
+  `,
+  body4: css`
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 140%; /* 16.8px */
+  `,
+  body5: css`
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 140%; /* 16.8px */
+  `,
+  caption: css`
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 140%; /* 14px */
+  `,
+}
