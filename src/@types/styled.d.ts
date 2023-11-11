5f09a6bc989b69825b409a277309ca2ed723dbc2
kroongft <51918951+kroongft@users.noreply.github.com>
2023-11-11 17:45:34 +0900

    chore: svgr 설치 및 설정

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,3 +5,15 @@ declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
+
+declare module '*.svg' {
+  const svg: string
+  export default svg
+}
+
+declare module '*.svg' {
+  import React from 'react'
+
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
+}
