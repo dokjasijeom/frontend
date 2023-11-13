0c313f221ceaa923e7bf92af37d3b0f0e37bac18
kroongft <51918951+kroongft@users.noreply.github.com>
2023-11-13 13:42:19 +0900

    add: Input 컴포넌트 추가

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const InputContainer = styled.div``
+
+const InputWrapper = styled.input`
+  padding: 12px 20px;
+  width: 100%;
+  border-radius: 12px;
+  border: solid 1px ${({ theme }) => theme.color.gray[200]};
+  color: ${({ theme }) => theme.color.gray[950]};
+  ${({ theme }) => theme.typography.body2};
+  outline: none;
+  background: ${({ theme }) => theme.color.system.w};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.gray[600]};
+  }
+  &:hover {
+    border: solid 1px ${({ theme }) => theme.color.gray[600]};
+  }
+  &:focus {
+    border: solid 2px ${({ theme }) => theme.color.main[600]};
+  }
+  &:disabled {
+    border: none;
+    background: ${({ theme }) => theme.color.gray[100]};
+  }
+  &.error {
+    border: solid 2px ${({ theme }) => theme.color.system.error} !important;
+  }
+`
+
+interface InputProps {
+  placeholder: string
+  disabled?: boolean
+  error?: boolean
+}
+
+function Input(props: InputProps) {
+  const { placeholder = '', disabled = false, error = false } = props
+  return (
+    <InputContainer>
+      <InputWrapper
+        type="text"
+        placeholder={placeholder}
+        disabled={disabled}
+        className={error ? 'error' : ''}
+      />
+    </InputContainer>
+  )
+}
+
+export default Input
