98f4541b8a46159a27af8ea1fe58fa47de9615a9
kroongft <51918951+kroongft@users.noreply.github.com>
2023-11-22 02:25:03 +0900

    add: OnlyFooterLayout 추가

---
+++ src/components/layout/OnlyFooterLayout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+import BottomBar from '../common/NavigationBar/BottomBar'
+
+const Centering = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const FixedWidth = styled.div`
+  width: 600px;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`
+interface OnlyFooterLayoutProps {
+  children: ReactNode | undefined
+}
+
+function OnlyFooterLayout(props: OnlyFooterLayoutProps) {
+  const { children } = props
+  return (
+    <Centering>
+      <FixedWidth>
+        {children}
+        <BottomBar />
+      </FixedWidth>
+    </Centering>
+  )
+}
+
+export default OnlyFooterLayout
