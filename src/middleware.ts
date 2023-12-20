c0f2c40afc4590d7207bb6f1b90b5002963b2c51
kroongft <51918951+kroongft@users.noreply.github.com>
2023-12-20 18:01:05 +0900

    refac: 미들웨어 로그인 후 조건 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,10 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // TODO: 로그인 여부 조건 필요
+  const isLogin = true
+  if (isLogin) {
+    return null
+  }
   return NextResponse.redirect(new URL('/auth/login', request.url))
 }
 
