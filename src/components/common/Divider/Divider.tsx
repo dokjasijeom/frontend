00b831712584efee3cd5c3298fafe15e5776124d
kroongft <51918951+kroongft@users.noreply.github.com>
2023-12-20 19:57:57 +0900

    add: Divider 컴포넌트 props 추가

--- src/components/common/Divider/Divider.tsx
+++ src/components/common/Divider/Divider.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-const DividerContainer = styled.div`
+const DividerContainer = styled.div<{
+  color: string
+  height: string
+}>`
   width: 100%;
-  height: 1px;
-  background: ${({ theme }) => theme.color.gray[200]};
+  height: ${({ height }) => height};
+  background: ${({ color, theme }) => color || theme.color.gray[200]};
   margin: 12px 0;
 `
 
-function Divider() {
-  return <DividerContainer />
+interface DividerProps {
+  size?: 'small' | 'medium' | 'large' | 'xlarge'
+  color?: string
+  [prop: string]: unknown
+}
+
+function Divider({ size = 'small', color = '#e1e2e4', ...rest }: DividerProps) {
+  const borderHeight = useMemo(() => {
+    if (size === 'small') return '1px'
+    if (size === 'medium') return '4px'
+    if (size === 'large') return '8px'
+    if (size === 'xlarge') return '12px'
+    return '1px'
+  }, [size])
+  return <DividerContainer color={color} height={borderHeight} {...rest} />
 }
 
 export default Divider
