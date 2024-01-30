a8f34732a6e523085f92f87f68bc90ad1394003d
kroongft <51918951+kroongft@users.noreply.github.com>
2024-01-30 22:01:02 +0900

    style: layout padding 수정

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -9,7 +9,7 @@ const Centering = styled.div`
 `
 const FixedWidth = styled.div`
   width: 600px;
-  padding: 56px 0 80px;
+  padding: 56px 0 88px;
   @media (max-width: 600px) {
     width: 100%;
   }
