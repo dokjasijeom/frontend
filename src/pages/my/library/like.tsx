a901ab4749a001aa8b55e8a6f6690075a5b9df84
kroongft <51918951+kroongft@users.noreply.github.com>
2024-02-06 23:36:47 +0900

    add: 찜한 작품이 없을 때 empty ui 추가

--- src/pages/my/library/like.tsx
+++ src/pages/my/library/like.tsx
@@ -2,6 +2,7 @@ import Tab from '@/components/common/Tab/Tab'
 import TitleHeader from '@/components/common/TitleHeader/TitleHeader'
 import OnlyFooterLayout from '@/components/layout/OnlyFooterLayout'
 import { CONTENTS_TAB_LIST } from '@/constants/Tab'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { ReactElement, useState } from 'react'
 import styled from 'styled-components'
@@ -11,7 +12,18 @@ const LikeContainer = styled.div``
 const LikeWrapper = styled.div`
   padding-top: 56px;
 `
+const LikeBookListWrapper = styled.div``
 
+const EmptyBook = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 140px;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  ${({ theme }) => theme.typography.body2};
+  color: ${({ theme }) => theme.color.gray[800]};
+`
 function Like() {
   const router = useRouter()
   const [selectedTab, setSelectedTab] = useState(CONTENTS_TAB_LIST[0])
@@ -25,6 +37,17 @@ function Like() {
           selectedTab={selectedTab}
           onChange={(tab) => setSelectedTab(tab)}
         />
+        <LikeBookListWrapper>
+          <EmptyBook>
+            <Image
+              src="/images/empty_book.png"
+              width={210}
+              height={105}
+              alt=""
+            />
+            찜한 작품이 없어요.
+          </EmptyBook>
+        </LikeBookListWrapper>
       </LikeWrapper>
     </LikeContainer>
   )
