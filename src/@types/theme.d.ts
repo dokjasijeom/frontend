57a047b86ea7ee51ee004fb5a49bb1dd6e321bfb
kroongft <51918951+kroongft@users.noreply.github.com>
2024-02-13 18:58:36 +0900

    chore: theme 타입 지정

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -1,14 +1,56 @@
+import { CSS } from 'styled-components/dist/types'
+
 export interface ColorType {
   main: {
-    [key: number]: string
+    50: string
+    100: string
+    200: string
+    300: string
+    400: string
+    500: string
+    600: string
+    700: string
+    800: string
+    900: string
+    950: string
   }
   sub: {
-    [key: number]: string
+    50: string
+    100: string
+    200: string
+    300: string
+    400: string
   }
   gray: {
-    [key: number]: string
+    50: string
+    100: string
+    200: string
+    300: string
+    600: string
+    800: string
+    950: string
   }
   system: {
+    w: string
+    bk: string
+    dim: string
+    error: string
+    success: string
+    naver: string
+    kakao: string
+    ridi: string
     [key: string]: string
   }
+}
+
+export interface TypographyType {
+  head1: CSS
+  head2: CSS
+  head3: CSS
+  body1: CSS
+  body2: CSS
+  body3: CSS
+  body4: CSS
+  body5: CSS
+  caption: CSS
 }
