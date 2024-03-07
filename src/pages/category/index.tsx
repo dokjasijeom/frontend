c2d00cf7c8591aeb50fe768276816ca50a80a556
kroongft <51918951+kroongft@users.noreply.github.com>
2024-03-07 21:17:05 +0900

    add: 카테고리 상단 탭 추가

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import Tab from '@/components/common/Tab/Tab'
+import TitleHeader from '@/components/common/TitleHeader/TitleHeader'
+import OnlyFooterLayout from '@/components/layout/OnlyFooterLayout'
+import { BOOK_TYPE_TAB_LIST } from '@/constants/Tab'
+import { useRouter } from 'next/router'
+import React, { ReactElement, useState } from 'react'
+import styled from 'styled-components'
+
+const CategoryContainer = styled.div``
+
+const CategoryWrapper = styled.div``
 
 function Category() {
-  return <div>Category</div>
+  const router = useRouter()
+  const [selectedTab, setSelectedTab] = useState(BOOK_TYPE_TAB_LIST[0])
+  return (
+    <CategoryContainer>
+      <TitleHeader
+        title="카테고리"
+        onClickBack={() => {
+          router.back()
+        }}
+      />
+      <CategoryWrapper>
+        <Tab
+          type="underbar"
+          tabList={BOOK_TYPE_TAB_LIST}
+          selectedTab={selectedTab}
+          onChange={(tab) => setSelectedTab(tab)}
+        />
+      </CategoryWrapper>
+    </CategoryContainer>
+  )
+}
+Category.getLayout = function getLayout(page: ReactElement) {
+  return <OnlyFooterLayout>{page}</OnlyFooterLayout>
 }
 
 export default Category
