52acf5e629b3c50b6c767457245efd1ea833c48b
kroongft <51918951+kroongft@users.noreply.github.com>
2024-04-01 22:20:45 +0900

    add: middleware isLogin 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // TODO: 로그인 여부 조건 필요
-  const isLogin = false
+
+  const isLogin = request.cookies.get('DS_AUT')?.value
   if (isLogin) {
     return null
   }
