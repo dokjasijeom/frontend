33857c408ecfedde2b6cc12becc91320dee9295b
kroongft <51918951+kroongft@users.noreply.github.com>
2024-04-03 19:01:17 +0900

    add: middleware 조건 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
+import { isEmpty } from 'lodash'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
+// eslint-disable-next-line consistent-return
 export function middleware(request: NextRequest) {
   // TODO: 로그인 여부 조건 필요
 
   const isLogin = request.cookies.get('DS_AUT')?.value
-  if (isLogin) {
-    return null
+  if (isEmpty(isLogin)) {
+    return NextResponse.redirect(new URL('/auth/login', request.url))
   }
-  return NextResponse.redirect(new URL('/auth/login', request.url))
 }
 
 // See "Matching Paths" below to learn more
