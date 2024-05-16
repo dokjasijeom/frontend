4557fb3052cb9b76106bccc3d94250322f2dd424
kroongft <51918951+kroongft@users.noreply.github.com>
2024-05-16 20:41:36 +0900

    chore: react-query 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import GlobalToast from '@/components/common/Toast/GlobalToast'
 import AppLayout from '@/components/layout/AppLayout'
 import GlobalStyle from '@/styles/GlobalStyle'
 import theme from '@/styles/theme'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -22,17 +23,20 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout || ((page) => <AppLayout>{page}</AppLayout>)
 
+  const queryClient = new QueryClient()
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>독자시점</title>
-        </Head>
-        <GlobalStyle />
-        <GlobalModal />
-        <GlobalToast />
-        {getLayout(<Component {...pageProps} />)}
-      </ThemeProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>독자시점</title>
+          </Head>
+          <GlobalStyle />
+          <GlobalModal />
+          <GlobalToast />
+          {getLayout(<Component {...pageProps} />)}
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   )
 }
