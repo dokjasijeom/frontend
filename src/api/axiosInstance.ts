26f49bf9e1c287f14171f9da72e8e842b9e556e1
kroongft <51918951+kroongft@users.noreply.github.com>
2024-05-16 20:42:14 +0900

    add: axios interceptors 설정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,26 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig } from 'axios'
+import { getCookie } from 'cookies-next'
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    'Content-Type': `application/json`,
+    'Content-Type': 'application/json',
   },
 })
+
+axiosInstance.interceptors.request.use(
+  (config): InternalAxiosRequestConfig => {
+    const token = getCookie('DS_AUT')
+
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error): any => {
+    return Promise.reject(error)
+  },
+)
 
 export default axiosInstance
