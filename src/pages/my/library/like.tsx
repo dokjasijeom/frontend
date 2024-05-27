0303d06c19b178f4ffa2646b2841b7bc6c3e9759
kroongft <51918951+kroongft@users.noreply.github.com>
2024-05-27 23:09:59 +0900

    add: 찜한 작품 데이터 호출 및 맵핑

--- src/pages/my/library/like.tsx
+++ src/pages/my/library/like.tsx
@@ -1,4 +1,4 @@
-import BookItem from '@/components/common/BookItem/BookItem'
+import { getUser } from '@/api/user'
 import Button from '@/components/common/Button/Button'
 import Divider from '@/components/common/Divider/Divider'
 import Icons from '@/components/common/Icons/Icons'
@@ -8,11 +8,14 @@ import OnlyFooterLayout from '@/components/layout/OnlyFooterLayout'
 import { MockMyBook } from '@/constants/MockData'
 import { SERIES_TYPE_TAB_LIST } from '@/constants/Tab'
 import useToast from '@/hooks/useToast'
+import { useQuery } from '@tanstack/react-query'
 import { isEmpty } from 'lodash'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { ReactElement, useState } from 'react'
 import styled, { useTheme } from 'styled-components'
+import SeriesItem from '@/components/common/SeriesItem/SeriesItem'
+import { Series } from '@/@types/series'
 
 const LikeContainer = styled.div``
 
@@ -52,6 +55,14 @@ function Like() {
   const theme = useTheme()
   const { showToast } = useToast()
 
+  const { data: user } = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const res = await getUser()
+      return res.data.data
+    },
+  })
+
   const handleAddMyLibrary = () => {
     showToast({ message: '기록장에 추가했어요!' })
   }
@@ -77,32 +88,34 @@ function Like() {
               찜한 작품이 없어요.
             </EmptyBook>
           )}
-          {MockMyBook.webNovel.map((book) => (
-            <>
-              <LikeBookWrapper key={book.id}>
-                <BookItem book={book} />
-                <Button
-                  width="auto"
-                  type="secondary"
-                  onClick={handleAddMyLibrary}
-                >
-                  <div className="add_button_body">
-                    <Icons
-                      width="20px"
-                      height="20px"
-                      name="Plus"
-                      color={theme.color.main[600]}
-                    />
-                    내 서재에 추가하기
-                  </div>
-                </Button>
-              </LikeBookWrapper>
-              <Divider
-                color={theme.color.gray[100]}
-                style={{ margin: '8px 0' }}
-              />
-            </>
-          ))}
+          {!isEmpty(user) &&
+            !isEmpty(user.likeSeries) &&
+            user.likeSeries.map((series: Series) => (
+              <>
+                <LikeBookWrapper key={series.hashId}>
+                  <SeriesItem series={series} />
+                  <Button
+                    width="auto"
+                    type="secondary"
+                    onClick={handleAddMyLibrary}
+                  >
+                    <div className="add_button_body">
+                      <Icons
+                        width="20px"
+                        height="20px"
+                        name="Plus"
+                        color={theme.color.main[600]}
+                      />
+                      내 서재에 추가하기
+                    </div>
+                  </Button>
+                </LikeBookWrapper>
+                <Divider
+                  color={theme.color.gray[100]}
+                  style={{ margin: '8px 0' }}
+                />
+              </>
+            ))}
         </LikeBookListWrapper>
       </LikeWrapper>
     </LikeContainer>
