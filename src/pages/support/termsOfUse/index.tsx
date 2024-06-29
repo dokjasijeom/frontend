87f49fd4d10a3003f26d09bcc88473db57005bcf
kroongft <51918951+kroongft@users.noreply.github.com>
2024-06-29 19:06:49 +0900

    add: 이용약관 페이지 헤더 수정

--- src/pages/support/termsOfUse/index.tsx
+++ src/pages/support/termsOfUse/index.tsx
@@ -1,8 +1,14 @@
+import TitleHeader from '@/components/common/TitleHeader/TitleHeader'
+import OnlyFooterLayout from '@/components/layout/OnlyFooterLayout'
 import { TERMS_OF_USE_TEXT_LIST } from '@/constants/TermsOfUse'
-import React from 'react'
+import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 
 const TermsOfUseContainer = styled.div`
+  padding-top: 56px;
+`
+
+const TermsOfUseWrapper = styled.div`
   padding: 16px 20px;
   ${({ theme }) => theme.color.gray[950]};
 
@@ -51,50 +57,59 @@ const TermsOfUseContainer = styled.div`
 function TermsOfUse() {
   return (
     <TermsOfUseContainer>
-      <div className="title">이용약관</div>
-      {TERMS_OF_USE_TEXT_LIST.map((text) => (
-        <>
-          <div className="sub_title">{text.title}</div>
-          <div className="body">
-            {text.list.map((li) => (
-              <>
-                <div>{li.title}</div>
-                {li.body.map((listBody, index) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <div className="text_list" key={index}>
-                    <div className="dot_text">
-                      {li.body.length > 1 && <div className="dot" />}
-                      <div
-                        className="text"
-                        style={{ paddingLeft: li.body.length > 1 ? '16px' : 0 }}
-                      >
-                        {listBody.desc}
-                      </div>
-                      {listBody.sub &&
-                        listBody.sub.map((listSub, listSubIndex) => (
-                          // eslint-disable-next-line react/no-array-index-key
-                          <div className="text_list sub" key={listSubIndex}>
-                            <div className="dot_text">
-                              <div className="dot" />
-                              <div className="text">{listSub}</div>
+      <TitleHeader title="이용약관" />
+      <TermsOfUseWrapper>
+        <div className="title">이용약관</div>
+        {TERMS_OF_USE_TEXT_LIST.map((text) => (
+          <>
+            <div className="sub_title">{text.title}</div>
+            <div className="body">
+              {text.list.map((li) => (
+                <>
+                  <div>{li.title}</div>
+                  {li.body.map((listBody, index) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <div className="text_list" key={index}>
+                      <div className="dot_text">
+                        {li.body.length > 1 && <div className="dot" />}
+                        <div
+                          className="text"
+                          style={{
+                            paddingLeft: li.body.length > 1 ? '16px' : 0,
+                          }}
+                        >
+                          {listBody.desc}
+                        </div>
+                        {listBody.sub &&
+                          listBody.sub.map((listSub, listSubIndex) => (
+                            // eslint-disable-next-line react/no-array-index-key
+                            <div className="text_list sub" key={listSubIndex}>
+                              <div className="dot_text">
+                                <div className="dot" />
+                                <div className="text">{listSub}</div>
+                              </div>
                             </div>
-                          </div>
-                        ))}
+                          ))}
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </>
-            ))}
-          </div>
-        </>
-      ))}
-      <div className="body">
-        부 칙 <br />
-        수정일 : 이 약관은 2024년 6월 3일 수정되었습니다. <br />
-        시행일 : 수정사항은 2024년 6월 3일부터 시행합니다.
-      </div>
+                  ))}
+                </>
+              ))}
+            </div>
+          </>
+        ))}
+        <div className="body">
+          부 칙 <br />
+          수정일 : 이 약관은 2024년 6월 3일 수정되었습니다. <br />
+          시행일 : 수정사항은 2024년 6월 3일부터 시행합니다.
+        </div>
+      </TermsOfUseWrapper>
     </TermsOfUseContainer>
   )
+}
+
+TermsOfUse.getLayout = function getLayout(page: ReactElement) {
+  return <OnlyFooterLayout>{page}</OnlyFooterLayout>
 }
 
 export default TermsOfUse
